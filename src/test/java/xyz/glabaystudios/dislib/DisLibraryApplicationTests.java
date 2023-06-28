73d8a0ff7d0de77e098b3d946da584042b2bd50f
Glabay <java_guru@live.com>
2023-06-28 08:52:48 -0300

    June 28th, 2023 - Fourth video
    Notes:
    - Implemented Discord
    
    Signed-off-by: Glabay <Java_Guru@live.com>

---
+++ src/test/java/xyz/glabaystudios/dislib/DisLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.glabaystudios.dislib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
